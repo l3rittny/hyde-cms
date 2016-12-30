dcc5ef494d7cfa8f5b9b9d8e1d561e98f1c54352
l3rittny <brittny.lapierre@gmail.com>
2016-12-30 18:54:55 -0500

    Edited app name

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -6,7 +6,7 @@ module.exports = React.createClass({
   render: function () {
     return (
     	<div className='header'>
-    	webpack react starter kit - header
+    		Hyde.io
     	</div>
 		)
 	}
